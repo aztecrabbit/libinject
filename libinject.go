a61e0510a1c127976c23f9e7dc4018df4325c62c
Aztec Rabbit <ars.xda@gmail.com>
2020-01-20 09:17:11 +0700

    ...

--- libinject.go
+++ libinject.go
@@ -22,6 +22,7 @@ type Inject struct {
     ProxyPort string
     ProxyPayload string
     ProxyTimeout int
+    LogConnecting bool
 }
 
 func (i *Inject) LogInfo(message string) {
@@ -41,14 +42,18 @@ func (i *Inject) Forward(c net.Conn) {
     buffer_size := make([]byte, 65535)
     length, _ := c.Read(buffer_size)
 
-    client_payload := string(buffer_size[:length])
-    client_payload_line_1 := strings.Split(client_payload, "\n")[0]
-    client_payload_host_port := strings.Split(client_payload_line_1, " ")[1]
-    client_payload_host_port_split := strings.Split(client_payload_host_port, ":")
-    i.LogInfo("Connecting -> " + client_payload_host_port_split[0] + " port " + client_payload_host_port_split[1])
+    if i.LogConnecting {
+        client_payload := string(buffer_size[:length])
+        client_payload_line_1 := strings.Split(client_payload, "\n")[0]
+        client_payload_host_port := strings.Split(client_payload_line_1, " ")[1]
+        client_payload_host_port_split := strings.Split(client_payload_host_port, ":")
+        i.LogInfo("Connecting -> " + client_payload_host_port_split[0] + " port " + client_payload_host_port_split[1])
+    }
 
     c.Write([]byte("HTTP/1.0 200 Connection established\r\n\r\n"))
     s.Write([]byte(i.ProxyPayload))
+
+    time.Sleep(200 * time.Millisecond)
 
     go io.Copy(s, c)
     io.Copy(c, s)
