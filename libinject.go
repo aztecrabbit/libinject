718625327d841323762c5b4dcb8973a2bde00f2c
Aztec Rabbit <ars.xda@gmail.com>
2020-01-26 01:14:16 +0700

    ...

--- libinject.go
+++ libinject.go
@@ -3,30 +3,36 @@ package libinject
 import (
     "os"
     "io"
-    "log"
+    "fmt"
     "net"
     "time"
+    "errors"
     "strings"
 
     "github.com/aztecrabbit/liblog"
+    "github.com/aztecrabbit/libredsocks"
 )
 
 var (
     Loop = true
-    ConfigDefault = &Config{
+    DefaultConfig = &Config{
+        Type: 2,
         Port: "8989",
-        ProxyHost: "202.152.240.50",
-        ProxyPort: "80",
+        Proxies: map[string][]string{
+        	"202.152.240.50:80": []string{
+                "*:*",
+            },
+        },
         ProxyPayload: "[raw][crlf]Host: t.co[crlf]Host: [crlf][crlf]",
-        ProxyTimeout: 10,
+        ProxyTimeout: 5,
         ShowLog: false,
     }
 )
 
 type Config struct {
+    Type int
     Port string
-    ProxyHost string
-    ProxyPort string
+    Proxies map[string][]string
     ProxyPayload string
     ProxyTimeout int
     ShowLog bool
@@ -34,85 +40,7 @@ type Config struct {
 
 type Inject struct {
     Config *Config
-}
-
-func (i *Inject) LogInfo(message string) {
-    liblog.LogInfo(message, "INFO", liblog.Colors["G1"])
-}
-
-func (i *Inject) LogReplace(message string) {
-    liblog.LogReplace(message, liblog.Colors["G2"])
-}
-
-func (i *Inject) Forward(c net.Conn) {
-    defer c.Close()
-
-    s, err := net.DialTimeout("tcp", i.Config.ProxyHost + ":" + i.Config.ProxyPort, time.Duration(i.Config.ProxyTimeout) * time.Second)
-    if err != nil {
-        liblog.LogException(err, "INFO")
-        return
-    }
-    defer s.Close()
-
-    buffer_size := make([]byte, 65535)
-    length, err := c.Read(buffer_size)
-    if err != nil {
-        panic(err)
-    }
-
-    client_payload := string(buffer_size[:length])
-    client_payload_line_1 := strings.Split(strings.Split(client_payload, "\r\n")[0], " ")
-    client_payload_method := client_payload_line_1[0]
-    client_payload_protocol := client_payload_line_1[2]
-    client_payload_host_port := strings.Split(client_payload_line_1[1], ":")
-    client_payload_host := client_payload_host_port[0]
-    client_payload_port := client_payload_host_port[1]
-
-    if client_payload_method != "CONNECT" {
-        liblog.LogInfo("Method not allowed", "INFO", liblog.Colors["R1"])
-        return
-    }
-
-    logConnectingMessage := "Connecting to " + client_payload_host + " port " + client_payload_port
-
-    if i.Config.ShowLog {
-        i.LogInfo(logConnectingMessage)
-    } else {
-        i.LogReplace(logConnectingMessage)
-    }
-
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[real_raw]", "[raw][crlf][crlf]")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[raw]", "[method] [host_port] [protocol]")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[method]", client_payload_method)
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[protocol]", client_payload_protocol)
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[host_port]", "[host]:[port]")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[host]", client_payload_host)
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[port]", client_payload_port)
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[crlf]", "[cr][lf]")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[lfcr]", "[lf][cr]")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[cr]", "\r")
-    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[lf]", "\n")
-
-    c.Write([]byte("HTTP/1.0 200 Connection established\r\n\r\n"))
-    s.Write([]byte(i.Config.ProxyPayload))
-
-    time.Sleep(200 * time.Millisecond)
-
-    done := make(chan bool)
-
-    go i.Handler(s, c, done)
-    go i.Handler(c, s, done)
-
-    <- done
-
-    c.Close()
-    s.Close()
-}
-
-func (i *Inject) Handler(w io.Writer, r io.Reader, done chan bool) {
-    io.Copy(w, r)
-
-    done <- true
+    Redsocks *libredsocks.Redsocks
 }
 
 func (i *Inject) Start() {
@@ -125,9 +53,169 @@ func (i *Inject) Start() {
     for {
         client, err := inject.Accept()
         if err != nil {
-            log.Fatalf("failed to accept listener %v", err)
+            panic(err)
         }
 
         go  i.Forward(client)
     }
+}
+
+func (i *Inject) ExtractClientRequest(c net.Conn) (map[string]string, error) {
+    buffer := make([]byte, 65535)
+    length, _ := c.Read(buffer)
+
+    request := make(map[string]string)
+    payload := strings.Split(string(buffer[:length]), "\r\n")
+    payloadHeader1 := strings.Split(payload[0], " ")
+
+    if len(payloadHeader1) < 3 {
+		return nil, errors.New("payload header 1 empty")
+    }
+
+    request["method"] = payloadHeader1[0]
+    if request["method"] != "CONNECT" {
+        return nil, errors.New("method not allowed!")
+    }
+
+    payloadHostPort := strings.Split(payloadHeader1[1], ":")
+    request["host"] = payloadHostPort[0]
+    request["port"] = payloadHostPort[1]
+
+   	request["protocol"] = payloadHeader1[2]
+
+    return request, nil
+}
+
+func (i *Inject) Forward(c net.Conn) {
+    defer c.Close()
+
+    request, err := i.ExtractClientRequest(c)
+    if err != nil {
+    	return
+    }
+
+    switch i.Config.Type {
+        case 2:
+            i.TunnelType2(c, request)
+
+        case 3:
+            i.TunnelType3(c, request)
+
+        default:
+            liblog.LogInfo("Inject type not found!", "INFO", liblog.Colors["R1"])
+    }
+
+    /*
+    if i.Config.Type == 2 {
+        i.TunnelType2(c, request)
+
+    } else if i.Config.Type == 3 {
+        i.TunnelType3(c, request)
+
+    } else {
+        liblog.LogInfo("Inject type not found!", "INFO", liblog.Colors["R1"])
+
+    }
+    */
+}
+
+func (i *Inject) GetProxy(request map[string]string) []string {
+    for proxyHostPort, whitelist_list := range i.Config.Proxies {
+        if len(whitelist_list) == 0 {
+            whitelist_list = []string{
+                "*:*",
+            }
+        }
+        for _, whitelist := range whitelist_list {
+            whitelistHostPort := strings.Split(whitelist, ":")
+            if len(whitelistHostPort) != 2 {
+                liblog.LogInfo("Inject whitelist error!", "INFO", liblog.Colors["R1"])
+                return nil
+            }
+            if (whitelistHostPort[0] == "*" || strings.Contains(request["host"], whitelistHostPort[0])) &&
+                    (whitelistHostPort[1] == "*" || whitelistHostPort[1] == request["port"]) {
+                return strings.Split(proxyHostPort, ":")
+            }
+        }
+    }
+
+    return nil
+}
+
+func (i *Inject) ProxyConnect(request map[string]string) (net.Conn, error) {
+    proxyHostPort := i.GetProxy(request)
+    if proxyHostPort == nil {
+        return nil, errors.New("Request blocked")
+    }
+
+    if i.Redsocks != nil {
+        i.Redsocks.RuleDirectAdd(proxyHostPort[0])
+    }
+
+    liblog.LogReplace(fmt.Sprintf(
+            "Connecting to %s port %s -> %s port %s", proxyHostPort[0], proxyHostPort[1], request["host"], request["port"],
+        ),
+        liblog.Colors["G2"],
+    )
+
+    return net.DialTimeout("tcp", strings.Join(proxyHostPort, ":"), time.Duration(i.Config.ProxyTimeout) * time.Second)
+}
+
+func (i *Inject) TunnelType2(c net.Conn, request map[string]string) {
+    s, err := i.ProxyConnect(request)
+    if err != nil {
+        return
+    }
+    defer s.Close()
+
+    logConnectingMessage := "Connecting to " + request["host"] + " port " + request["port"]
+
+    if i.Config.ShowLog {
+        liblog.LogInfo(logConnectingMessage, "INFO", liblog.Colors["G1"])
+    }
+
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[real_raw]", "[raw][crlf][crlf]")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[raw]", "[method] [host_port] [protocol]")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[method]", request["method"])
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[host_port]", "[host]:[port]")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[host]", request["host"])
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[port]", request["port"])
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[protocol]", request["protocol"])
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[crlf]", "[cr][lf]")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[lfcr]", "[lf][cr]")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[cr]", "\r")
+    i.Config.ProxyPayload = strings.ReplaceAll(i.Config.ProxyPayload, "[lf]", "\n")
+
+    s.Write([]byte(i.Config.ProxyPayload))
+
+    i.Handler(c, s)
+}
+
+func (i *Inject) TunnelType3(c net.Conn, request map[string]string) {
+    s, err := i.ProxyConnect(request)
+    if err != nil {
+        return
+    }
+    defer s.Close()
+
+    i.Handler(c, s)
+}
+
+func (i *Inject) Handler(c net.Conn, s net.Conn) {
+    c.Write([]byte("HTTP/1.0 200 Connection established\r\n\r\n"))
+
+    time.Sleep(200 * time.Millisecond)
+
+    done := make(chan bool)
+
+    go i.Handle(s, c, done)
+    go i.Handle(c, s, done)
+
+    <- done
+}
+
+func (i *Inject) Handle(w io.Writer, r io.Reader, done chan bool) {
+    io.Copy(w, r)
+
+    done <- true
 }
